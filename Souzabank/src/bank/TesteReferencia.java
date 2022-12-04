5ba56d9616217e3aa4ff1d3baf90298c2f3cb0ca
Leniza <lenizarbsz@gmail.com>
2022-12-03 23:29:06 -0300

    exercício Orientação a Objeto

---
+++ Souzabank/src/bank/TesteReferencia.java
@@ -0,0 +1,26 @@
+package bank;
+
+public class TesteReferencia {
+	
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("saldo da primeira conta: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;		
+		System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("saldo da segunda conta: " + segundaConta.saldo);
+		
+		System.out.println("saldo da primeira conta: " +primeiraConta.saldo);
+		
+		if(primeiraConta == segundaConta) {
+			System.out.println("são as mesmissima conta");
+		}
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+	}
+	
+}
