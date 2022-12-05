3c58f0ac84d794723e189e76b60241cacfa35b8a
Leniza <lenizarbsz@gmail.com>
2022-12-04 22:42:36 -0300

    correcao do cadastro

--- Souzabank/src/bank/TestaMetodo.java
+++ Souzabank/src/bank/TestaMetodo.java
@@ -3,26 +3,29 @@ package bank;
 public class TestaMetodo {
 
 	public static void main(String[] args) {
-		Conta contaDoPaulo = new Conta();
-		contaDoPaulo.saldo = 100;
-		contaDoPaulo.deposita(50);
-		System.out.println(contaDoPaulo.saldo);
+		Conta contaDaRegina = new Conta();
+		contaDaRegina.saldo = 100;
+		contaDaRegina.deposita(50);
+		System.out.println(contaDaRegina.saldo);
 		
-		boolean conseguiuRetirar = contaDoPaulo.saca(20);
-		System.out.println(contaDoPaulo.saldo);
+		boolean conseguiuRetirar = contaDaRegina.saca(20);
+		System.out.println(contaDaRegina.saldo);
 		System.out.println(conseguiuRetirar);
 		
 		Conta contaDaMarcela = new Conta();
 		contaDaMarcela.deposita(1000);
 		boolean sucessoTransferencia = 
-				contaDaMarcela.transfere(3000, contaDoPaulo);
+				contaDaMarcela.transfere(3000, contaDaRegina);
 		if(sucessoTransferencia) {
 			System.out.println("transferencia com sucesso");
 		} else {
 			System.out.println("faltou dinheiro");
 		}
 		System.out.println(contaDaMarcela.saldo);
-		System.out.println(contaDoPaulo.saldo);
+		System.out.println(contaDaRegina.saldo);
+		
+		contaDaRegina.titular = "regina bou";
+		System.out.println(contaDaRegina.titular);
 	}
 
 }
