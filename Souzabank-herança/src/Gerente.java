0b9f9b788d4d955227f205a8bd0e1ca0b5bf3623
Leniza <lenizarbsz@gmail.com>
2022-12-12 18:07:18 -0300

    praticando herança e interface

--- Souzabank-herança/src/Gerente.java
+++ Souzabank-herança/src/Gerente.java
@@ -2,24 +2,23 @@
 //Gerente eh um Funcionario ou Gerente herda da classe do funcionario, assina o contrato Autenticável, eh um autenticavel.
 public class Gerente extends Funcionario implements Autenticavel {
 	
-	private int senha;		
+	private AutenticaUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticaUtil();
+	}			
 		
 	public double getBonificacao() {
 		System.out.println("Chamando o metodo bonificacao do Gerente");
 		return super.getSalario();
 	}
-	
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;		
+		this.autenticador.setSenha(senha);		
 	}
 
 	@Override
 	public boolean autentica(int senha) {		
-			if(this.senha == senha) {
-				return true;
-			} else {
-				return false;
-			}
+		return this.autenticador.autentica(senha);	
 		}	
 }
