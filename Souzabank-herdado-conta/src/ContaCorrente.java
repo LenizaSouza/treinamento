ff04e1aa213a334dd18cab12d6506cd1ea855b99
Leniza <lenizarbsz@gmail.com>
2022-12-13 17:26:14 -0300

    heranca

--- Souzabank-herdado-conta/src/ContaCorrente.java
+++ Souzabank-herdado-conta/src/ContaCorrente.java
@@ -1,5 +1,5 @@
 //new ContaCorrente
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
@@ -8,15 +8,17 @@ public class ContaCorrente extends Conta {
 	@Override
 	public boolean saca(double valor) {
 		double valorASacar = valor + 0.2;
-		return super.saca(valorASacar);
-		
-		
+		return super.saca(valorASacar);		
 	}
 
 	@Override
 	public void deposita(double valor) {
-		super.saldo += valor;
-		
+		super.saldo += valor;		
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
 	}
 
 
