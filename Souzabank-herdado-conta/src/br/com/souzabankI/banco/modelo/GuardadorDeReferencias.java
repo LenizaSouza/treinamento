c3b6b7eecafb5fd04aae9ca741c8d5b13d9d6fd4
Leniza Souza <lenizarb@gmail.com>
2023-01-01 22:16:02 -0300

    ArrayList

---
+++ Souzabank-herdado-conta/src/br/com/souzabankI/banco/modelo/GuardadorDeReferencias.java
@@ -0,0 +1,27 @@
+package br.com.souzabankI.banco.modelo;
+
+public class GuardadorDeReferencias {
+	
+	private Conta[] referencias;
+	private int posicaoLivre;
+	
+	public GuardadorDeReferencias() {
+		this.referencias = new Object[10];
+		this.posicaoLivre = 0;
+	}
+	
+	public void adiciona(Object ref) {
+		this.referencias[this.posicaoLivre] = ref;
+		this.posicaoLivre++;
+		
+	}
+
+	public int getQuantidadeDeElementos() {
+		return this.posicaoLivre;
+	}
+
+	public Object getReferencia(int pos) {
+		return this.referencias[pos];
+	}
+
+}
