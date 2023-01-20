abb00feea063b9ed83b0e394b965a19866973190
Leniza Souza <lenizarb@gmail.com>
2023-01-20 20:50:26 -0300

    metodods da classe integer

--- Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
+++ Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
@@ -11,7 +11,18 @@ public class Teste {
 	String[] nomes = new String[5];
 	
 	int idade = 29;//Integer
-	Integer idadeRef = new Integer(29);
+	
+	Integer idadeRef = Integer.valueOf(29);
+	int valor = idadeRef.intValue();
+	
+	String s = args[0];//"10"
+	
+	Integer numero = Integer.valueOf(s);
+	System.out.println(numero);
+
+	
+	
+	
 	List<Integer> numeros = new ArrayList<Integer>();
 	numeros.add(29);//Autoboxing
 	
