b2d810e35484b023ac993930a7e7cc703e6101ec
Leniza Souza <lenizarb@gmail.com>
2023-01-21 19:35:42 -0300

    métodos da classe Integer

--- Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
+++ Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
@@ -6,26 +6,25 @@ import java.util.List;
 public class Teste {
 	
 	public static void main(String[] args) {
-	
-	int[] idades = new int [5];
-	String[] nomes = new String[5];
-	
+		
 	int idade = 29;//Integer
+	Integer idadeRef = Integer.valueOf(29);//autoboxing	
+	System.out.println(idadeRef.doubleValue());
 	
-	Integer idadeRef = Integer.valueOf(29);
-	int valor = idadeRef.intValue();
+	System.out.println(Integer.MAX_VALUE);
+	System.out.println(Integer.MIN_VALUE);
 	
-	String s = args[0];//"10"
+	System.out.println(Integer.SIZE);
+	System.out.println(Integer.BYTES);
 	
-	Integer numero = Integer.valueOf(s);
-	System.out.println(numero);
-
-	
-	
+	int valor = idadeRef.intValue();//unboxing
+	String s = args[0];//"10"	
+	//Integer numero = Integer.valueOf(s);
+	int numero = Integer.parseInt(s);
+	System.out.println(numero);	
 	
 	List<Integer> numeros = new ArrayList<Integer>();
-	numeros.add(29);//Autoboxing
-	
+	numeros.add(29);//Autoboxing	
 	
 	}
 }
