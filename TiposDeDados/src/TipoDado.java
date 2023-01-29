7e681ba846e11b1356a2e068e19718f3138958fb
Leniza Souza <lenizarb@gmail.com>
2023-01-28 21:46:09 -0300

    java fundamentos

---
+++ TiposDeDados/src/TipoDado.java
@@ -0,0 +1,21 @@
+
+public class TipoDado {
+
+	public static void main(String[] args) {
+		// demonstração de alguns tipos de dados
+		int numero;
+		float decimalCurto;
+		double decimalLongo;
+		char caracter;
+		String cadeiacaracteres;
+		
+		numero = 10;
+		decimalCurto = 10.5f;
+		decimalLongo = 20.5;
+		caracter ='F';
+		cadeiacaracteres = "** Meu Curso de Java Fundamentos em 2023 **";
+		
+
+	}
+
+}
