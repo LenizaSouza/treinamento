b724491d720f17f3a482ec6dd02466e63be566b1
Leniza Souza <lenizarb@gmail.com>
2023-01-30 15:22:35 -0300

    fundamentos java

---
+++ Literais/src/Literal.java
@@ -0,0 +1,24 @@
+
+public class Literal {
+
+	
+	public static void main(String[] args) {
+		int numero;
+		String cadeiaCaracteres;
+		char caracter;
+		boolean certoOuErrado;
+		
+		numero = 10;
+		cadeiaCaracteres = "Meu curso ";
+		caracter = 'S';
+		certoOuErrado = true;
+		
+		System.out.println("O valor de numero é: " + numero);
+		System.out.println("O valor de cadeiacaracter é: " + cadeiaCaracteres);
+		System.out.println("O valor de caracter é: " + caracter);
+		System.out.println("O valor de CertoOuErrado é: " + certoOuErrado);
+		
+		
+		
+	}
+}
