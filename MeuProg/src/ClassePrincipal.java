0074af91480d2f3eecb4480ff9714226f9f50b1f
Leniza Souza <lenizarb@gmail.com>
2023-02-03 13:30:40 -0300

    java fundamentos

---
+++ MeuProg/src/ClassePrincipal.java
@@ -0,0 +1,10 @@
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		float numero = 0.5f;
+		numero = 10.5f;		
+		System.out.println("Valor de número é: " + numero);
+	}
+
+}
