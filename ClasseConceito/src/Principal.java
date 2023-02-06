a34044ade8c8f58f4716a4cbdf2478b8386a839b
Leniza Souza <lenizarb@gmail.com>
2023-02-06 11:30:58 -0300

    java fundamentos

---
+++ ClasseConceito/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pessoa pes = new Pessoa();
+		pes.setNomePessoa("João do Nascimento");
+		pes.setIdadePessoa(15);
+		System.out.println(pes.toString());
+
+	}
+
+}
