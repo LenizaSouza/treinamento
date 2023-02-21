2cd6f8a9d82e5c969eb296201313e2f5ed0bc748
Leniza Souza <lenizarb@gmail.com>
2023-02-21 19:51:40 -0300

    classes anonimas

--- Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
+++ Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
@@ -44,22 +44,22 @@ public class Teste {
 		lista.add(cc3);
 		lista.add(cc4);
 		
-		for (Conta conta : lista) {
-			System.out.println(conta);
-		}
-		
-		//NumeroDaContaComparator comparator = new NumeroDaContaComparator();
-		lista.sort(null);
-		
-		//Collections.sort(lista, new NumeroDaContaComparator());
-		//Collections.sort(lista);
-		//Collections.reverse(lista);
-		
-		System.out.println("---------");
+		NumeroDaContaComparator2 comparator = new NumeroDaContaComparator2();
+		lista.sort(comparator);
 		
 		for (Conta conta : lista) {
 			System.out.println(conta + ", " + conta.getTitular().getNome());
 		}
+	}
+}
+
+class NumeroDaContaComparator2 implements Comparator<Conta> {
+
+	@Override
+	public int compare(Conta c1, Conta c2) {
+		return Integer.compare(c1.getNumero(), c2.getNumero());
+		
+
 	}
 }
 
@@ -73,20 +73,3 @@ class TitularDaContaComparator2 implements Comparator<Conta> {
 	}
 }
 
-class NumeroDaContaComparator2 implements Comparator<Conta> {
-
-	@Override
-	public int compare(Conta c1, Conta c2) {
-		return Integer.compare(c1.getNumero(), c2.getNumero());
-		
-//		return c1.getNumero() - c2.getNumero();
-		
-//		if(c1.getNumero() < c2.getNumero()) {
-//			return -1;
-//		}
-//		if(c1.getNumero() > c2.getNumero()) {
-//			return 1;
-//		}
-//		return 0;
-	}
-}
