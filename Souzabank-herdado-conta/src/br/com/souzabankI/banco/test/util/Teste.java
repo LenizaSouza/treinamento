144e5d39aea3e1f1b1bb23d0b364ba097c338bd0
Leniza Souza <lenizarb@gmail.com>
2023-02-23 18:15:08 -0300

    classes anonimas

--- Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
+++ Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
@@ -44,7 +44,7 @@ public class Teste {
 		lista.add(cc3);
 		lista.add(cc4);
 		
-		lista.sort(new Comparator<Conta>() {
+		lista.sort(new Comparator<Conta>() { //classe anonima
 
 				@Override
 				public int compare(Conta c1, Conta c2) {
@@ -53,29 +53,18 @@ public class Teste {
 			}				
 		);
 		
+		Comparator<Conta> comp = new Comparator<Conta>() {
+
+			@Override
+			public int compare(Conta c1, Conta c2) {
+				String nomeC1 = c1.getTitular().getNome();
+				String nomeC2 = c2.getTitular().getNome();
+				return nomeC1.compareTo(nomeC2);
+			}
+		};
+		
 		for (Conta conta : lista) {
 			System.out.println(conta + ", " + conta.getTitular().getNome());
 		}
 	}
 }
-
-class NumeroDaContaComparator2 implements Comparator<Conta> {
-
-	@Override
-	public int compare(Conta c1, Conta c2) {
-		return Integer.compare(c1.getNumero(), c2.getNumero());
-		
-
-	}
-}
-
-class TitularDaContaComparator2 implements Comparator<Conta> {
-
-	@Override
-	public int compare(Conta c1, Conta c2) {
-		String nomeC1 = c1.getTitular().getNome();
-		String nomeC2 = c2.getTitular().getNome();
-		return nomeC1.compareTo(nomeC2);
-	}
-}
-
