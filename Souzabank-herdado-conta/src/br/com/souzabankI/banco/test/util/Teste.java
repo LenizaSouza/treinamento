efa40f98c19723a59e61d4a994165dbda91d3a66
Leniza Souza <lenizarb@gmail.com>
2023-02-25 21:33:41 -0300

    lambdas

--- Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
+++ Souzabank-herdado-conta/src/br/com/souzabankI/banco/test/util/Teste.java
@@ -49,14 +49,10 @@ public class Teste {
 				}				
 		);
 		
-		Comparator<Conta> comp = new Comparator<Conta>() {
-
-			@Override
-			public int compare(Conta c1, Conta c2) {
+		Comparator<Conta> comp = (Conta c1, Conta c2) -> {
 				String nomeC1 = c1.getTitular().getNome();
 				String nomeC2 = c2.getTitular().getNome();
-				return nomeC1.compareTo(nomeC2);
-			}
+				return nomeC1.compareTo(nomeC2);			
 		};
 		
 		for (Conta conta : lista) {
