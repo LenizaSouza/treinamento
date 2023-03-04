223c21763210db200f58b136c61e5cad3cb3dc21
Leniza Souza <lenizarb@gmail.com>
2023-03-04 18:51:09 -0300

    Fundamentos de CSS

---
+++ Treinamento CSS/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/page2', function(req, res){
+    res.render("../views/page2");
+});
+
+app.get('/', function(req, res) {
+    res.render("../views/index");
+});
+
+app.get('/offlinePage', function(req, res) {
+    res.render("../views/offlinePage");
+});
+
+app.listen(3000, function(){
+    console.log('Executando na porta 3000');
+});
