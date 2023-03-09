14b8a4fe92f0683071f4b090c9bf9d891f02aa51
Leniza Souza <lenizarb@gmail.com>
2023-03-09 18:11:43 -0300

    operadores matemáticos em less

--- ExemploPWA/index.js
+++ ExemploPWA/index.js
@@ -4,18 +4,10 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/page2', function(req, res){
-    res.render("../views/page2");
-});
-
 app.get('/', function(req, res) {
-    res.render("../views/index");
+    res.render("less");
 });
 
-app.get('/offlinePage', function(req, res) {
-    res.render("../views/offlinePage");
-});
-
-app.listen(3000, function(){
-    console.log('Executando na porta 3000');
+app.listen(3001, function(){
+    console.log('Executando na porta 3001');
 });
